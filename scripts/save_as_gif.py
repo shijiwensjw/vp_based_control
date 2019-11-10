e801b14f1893200e922621a2e1bba544b0c4a8f8
steven_ur <sjwimuzdh@163.com>
2019-11-10 22:49:44 +0800

    small modify for save_video_gif

--- scripts/save_as_gif.py
+++ scripts/save_as_gif.py
@@ -16,26 +16,28 @@ def save_video_gif(filename, frames):
 def save_video(grp, num):
     imlist = []
     for t in range(31):
-        image_folder = '/home/steven/Project/graduate_design/vp_based_control/data_collection/sawyer_robot/autograsp_env/sudri/train/traj_group{}/traj{}'.format(str(grp), str(num)) + '/images0'
+        image_folder = '/home/ur/sjw_gd/graduate_design/vp_based_control/data_collection/sawyer_robot/autograsp_env/sudri/train/traj_group{}/traj{}'.format(str(grp), str(grp*1000+num)) + '/images0'
         # print('image_folder: ',image_folder)
         try:
             [imfile] = glob.glob(image_folder + "/im_{}.jpg".format(str(t)))
-        except Exsception:
+            image = np.asarray(Image.open(imfile))
+
+            imlist.append(image)
+        except Exception:
             print('No such file')
         # imfile = image_folder + "/im_{}.jpg".format(str(t))
         # print('imfile:', imfile)
-        image = np.asarray(Image.open(imfile))
 
-        imlist.append(image)
 
-    save_dir = '/home/steven/Project/graduate_design/vp_based_control/data_collection/sawyer_robot/autograsp_env/sudri/rawdata_video/traj_group{}'.format(str(grp))
+    save_dir = '/home/ur/sjw_gd/graduate_design/vp_based_control/data_collection/sawyer_robot/autograsp_env/sudri/rawdata_video/traj_group{}'.format(str(grp))
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     save_video_gif(save_dir+'/traj{}'.format(num), imlist)
 
 def main():
-    for grp in range(10):
-        for num in range(10):
+    for grp in range(13,14):
+        for num in range(1000):
+            print('saving group_{}, traj_{}'.format(str(grp), str(grp*1000+num)))
             save_video(grp, num)
 if __name__ == '__main__':
     main()
